19853a103050802e99a91780ba134520dbc7a63f
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-12-16 14:50:19 -0600

    Added some examples on how to use dynamic arrays.

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -3,24 +3,38 @@
 
 int main()
 {
-    DynamicArray<int> intArray;
-    intArray.Add(1);
-    intArray.Add(109);
-    intArray.Add(1090);
+    class Actor {};
+    class Component {};
 
-    for (int i = 0; i < intArray.Length(); i++)
-    {
-        // Do stuff
-    }
+    DynamicArray<int> x;
+    DynamicArray<int> y = x;
 
-    intArray.RemoveIndex(2);
-    intArray.Remove(1090);
-    std::cout << std::endl;
+    
 
-    for (int element : intArray)
-    {
-        std::cout << element << std::endl;
-    }
+
+    DynamicArray<Actor*>* actorArray = new DynamicArray<Actor*>();
+    DynamicArray<Component*>* componentArray = new DynamicArray<Component*>();
+    actorArray->Add(new Actor());
+    delete actorArray;
+
+    //DynamicArray<int> intArray;
+    //intArray.Add(1);
+    //intArray.Add(109);
+    //intArray.Add(1090);
+
+    //for (int i = 0; i < intArray.Length(); i++)
+    //{
+    //    // Do stuff
+    //}
+
+    //intArray.RemoveIndex(2);
+    //intArray.Remove(1090);
+    //std::cout << std::endl;
+
+    //for (int element : intArray)
+    //{
+    //    std::cout << element << std::endl;
+    //}
 
     return 0;
 }
